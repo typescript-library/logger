af35ce6a192fd3d340ca8f22afd8a5478053d01b
Edwin.JH.Lee <edwin.jh.lee@gmail.com>
2018-04-22 15:36:07 +0800

    ChalkSerializer about #3

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,17 +1,38 @@
 import { Logger } from "./index"
 
+import * as s from "./serialize"
+
 function main() {
-    const llo = new Logger();
+    const llo = new Logger(new s.ChalkSerializer());
     llo.debug.o({
         msg: "Program ready"
     });
 
     llo.debug.msg("123");
     llo.debug.msg_data("123", {
-        status: "on"
+        status: "on",
+        work: {
+            a: 1,
+            b: 2
+        }
     });
 
     llo.warn.trace(new Error("Here").stack || "Here")
+    llo.fatal.msg_data("Wrong", {
+        status: "off",
+        work: {
+            a: 3,
+            b: 4
+        }
+    })
+
+    llo.error.msg_data("Wrong", {
+        status: "off",
+        work: {
+            a: 3,
+            b: 4
+        }
+    })
 
 }
 
--- src/serialize.ts
+++ src/serialize.ts
@@ -51,3 +51,65 @@ export class DefaultSerializer extends SERIALIZER_TYPE {
     }
 
 }
+
+import { default as c } from "chalk"
+
+const fmap = {
+    0: c.green,
+    1: c.blue,
+    2: c.yellow,
+    3: c.red,
+    4: c.magenta
+}
+
+export function convert(data: { [index: string]: any }, ret: Array<string>, prefix = ""){
+    for (const i in data) {
+        if (typeof(data[i]) !== "object"){
+            ret.push(`[${prefix+i}]=${data[i]}`)
+        } else {
+            convert(data[i], ret, prefix+i+".")
+        }
+    }
+}
+
+export class ChalkSerializer extends DefaultSerializer {
+
+    static SEP = 9
+    static LEADING_SPACE = " ".repeat(ChalkSerializer.SEP + 1)
+    static LEADING_CHARS = "_".repeat(ChalkSerializer.SEP)
+
+    constructor(write: (msg: string)=> void = console.log.bind(console)){
+        super()
+    }
+
+    history = Date.now()
+    public log(
+        logType: t.LevelType, 
+        o: t.LevelLoggerOption
+    ): void {
+        const general_text_fun = fmap[logType]
+
+        const now = Date.now()
+
+        const diff_time_str = (
+            ChalkSerializer.LEADING_CHARS + 
+            (now - this.history) 
+        ).slice(-ChalkSerializer.SEP)
+        
+        const l_difftime = c.redBright(c.bgWhite(diff_time_str))
+        const l_time = c.bgWhite(c.black(new Date(now).toISOString() ) )
+        const l_msg = general_text_fun(o.msg || "")
+        let msg = `${l_difftime} ${l_time} ${l_msg}`
+        
+
+        if (o.data){
+            const ret: Array<string> = []
+            convert(o.data, ret, "")
+            const l_data = general_text_fun(ret.join(" "))
+            msg += `\n${l_data}`
+        }
+        this.write(msg.replace("\n", `\n${ChalkSerializer.LEADING_SPACE}`))
+        
+        this.history = now
+    }
+}
