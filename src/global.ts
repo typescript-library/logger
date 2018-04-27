5df7a5c8b67865621968ed8b4a5b7b7f89478204
Edwin.JH.Lee <edwin.jh.lee@gmail.com>
2018-04-27 11:18:08 +0800

    Close #7

--- src/global.ts
+++ src/global.ts
@@ -1,18 +1,15 @@
 import * as index from "./index"
 
-export var RootLogger = index.Logger.createRoot(
-    "Logger",
-    new index.Serializer.Major(
-        index.Stringify.createChalk(),
-        index.Output.combine(
-            index.Output.CONSOLE,
-            // index.Output.file("/tmp/a.txt")
+export class Instances {
+    public static RootLogger = index.Logger.createRoot(
+        "Logger",
+        new index.Serializer.Major(
+            index.Stringify.createChalk(),
+            index.Output.combine(
+                index.Output.CONSOLE,
+                // index.Output.file("/tmp/a.txt")
+            )
         )
     )
-)
-
-
-export function injectLogger(logger: index.Logger){
-    RootLogger = logger
 }
 
